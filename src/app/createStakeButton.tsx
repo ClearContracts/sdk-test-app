20885abf7cde703b71fb9616a74a01e517ec1306
Matthew Laux <mlaux6@outlook.com>
2025-04-04 14:03:21 +1100

    Change createStakeButton amount to 10 tokens

--- src/app/createStakeButton.tsx
+++ src/app/createStakeButton.tsx
@@ -2,7 +2,7 @@ import ClaritySDK from "@claritydao/clarity-sdk";
 
 const CreateStakeButton = () => {
   const handleClick = async () => {
-    const txId = await ClaritySDK.createStake(3000000);
+    const txId = await ClaritySDK.createStake(10000000);
     console.log("Create stake TX ID:", txId);
   };
   return <button onClick={handleClick}>Create Stake</button>;
